fd636b43d8d3f7e3fd5216f797118522c515bdda
bonugopal <bonugopal@capgemini.com>
2023-04-19 14:30:47 +0530

    ProjectList-Added useSelector

--- src/components/Projects/MyProjects/ProjectList.js
+++ src/components/Projects/MyProjects/ProjectList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef, Suspense } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { ProjectService } from "../../../service/PegaService";
