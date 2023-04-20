026a4077cfdd825f5925cffea8887b0a804fa63e
Rajansingh97 <rajan-kumar.a.singh@capgemini.com>
2023-04-20 16:23:27 +0530

    sidebar expandview changes and header changes

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -3,13 +3,11 @@ import "./index.scss";
 import Notificaitons from "../../assets/images/notification.svg";
 import user from "../../assets/images/user.svg";
 
-
 const sessionData = sessionStorage.getItem("session");
 
 const sessionObj = JSON.parse(sessionData);
 
 const time = sessionObj.loginTime;
-
 
 //for userid and username
 const username = sessionObj.username;
@@ -17,17 +15,14 @@ const username = sessionObj.username;
 const userid = sessionObj.userid;
 
 
-
 const Header = () => {
   return (
-      
     <div className="header">
-      
       <div>
-        <h1>Welcome back, {username}!</h1>
+        <h1>Welcome Back, {username}!</h1>
         <div className="user-date-time">
           <p>{time}</p>
-          {/* <p>03:10 pm (GMT+5:30)</p> */}
+          <p>03:10 pm (GMT+5:30)</p>
         </div>
       </div>
       <div className="user-profile">
@@ -37,7 +32,7 @@ const Header = () => {
           className="notificaitons"
         />
         <p>
-        {username}
+         {username}
           <span>{userid}</span>
         </p>
         <img src={user} alt="user profile" className="userProfile" />
@@ -46,5 +41,3 @@ const Header = () => {
   );
 };
 export default Header;
-
-
