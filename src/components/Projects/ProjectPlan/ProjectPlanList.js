b43d2a031e7de22ac7e9fc8e0102afec131b8fa4
bonugopal <bonugopal@capgemini.com>
2023-04-25 15:53:49 +0530

    ProjectPlanList-Mock data integration.

--- src/components/Projects/ProjectPlan/ProjectPlanList.js
+++ src/components/Projects/ProjectPlan/ProjectPlanList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef, Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DataTable } from "primereact/datatable";
 import { TreeTable } from "primereact/treetable";
 import { Column } from "primereact/column";
 import { ProjectService } from "../../../service/PegaService";
@@ -22,7 +21,7 @@ const ProjectPlanList = (props) => {
   const [selectedCities, setSelectedCities] = useState([]);
   const [filters, setFilters] = useState([]);
   const [sortData, setSortData] = useState([]);
-  const [isSearch, isSearchSet] = useState(false);
+  const [isSearch] = useState(false);
 
   const myProjectList = useSelector((state) => state.myProject);
   console.log("myProjectList:", myProjectList);
@@ -63,11 +62,9 @@ const ProjectPlanList = (props) => {
   };
 
   useEffect(() => {
-    if (!myProjectList.projectPlan.length) {
-      const updatedUsers = dispatch(projectPlan());
-      console.log("projectPlan::", updatedUsers);
-    }
-  }, [dispatch, myProjectList.projectPlan]);
+    const updatedUsers = dispatch(projectPlan());
+    console.log("projectPlan::", updatedUsers);
+  }, [dispatch]);
 
   useEffect(() => {
     (async () => {
@@ -243,32 +240,6 @@ const ProjectPlanList = (props) => {
       </>
     );
   };
-  const allowExpansion = (rowData) => {
-    if (rowData.SubTask) return rowData.SubTask.length > 0;
-    else return true;
-  };
-  const dynamicColumns = () => {
-    if (projectColumnName.length) {
-      return projectColumnName.map((ele, i) => {
-        return (
-          <Column
-            key={ele}
-            field={ele}
-            filterField={ele}
-            header={projectNameHeader(ele)}
-            columnKey={i}
-            frozen={frozenCoulmns.includes(ele)}
-            alignFrozen="left"
-            className={frozenCoulmns.includes(ele) ? "font-bold" : ""}
-            filter
-            showFilterMenu={false}
-            filterPlaceholder={ele}
-            body={fullKitReadinessBody}
-          />
-        );
-      });
-    }
-  };
 
   const onGlobalFilterChange = (e) => {
     const value = e.value;
@@ -342,21 +313,20 @@ const ProjectPlanList = (props) => {
 
   const [expandedRows, setExpandedRows] = useState(null);
   const rowExpansionColumnNames = [
-    "Test",
     "Task",
     "Dependancy",
     "Role",
     "Owner",
     "State",
-    "Duration(Days)",
-    "Start Date",
-    "End Date",
-    "Consumed Buffer",
-    "Help Needed",
+    "Duration",
+    "StartDate",
+    "EndDate",
+    "ConsumedBuffer",
+    "HelpNeeded",
   ];
 
   const rowExpansionHeader = (options) => {
-    return <div style={{ display: "none" }}>{options}</div>;
+    return <div>{options}</div>;
   };
   const rowExpansionColumns = () => {
     if (rowExpansionColumnNames.length) {
@@ -365,455 +335,67 @@ const ProjectPlanList = (props) => {
           <Column
             key={ele}
             field={ele}
-            header={rowExpansionHeader(ele)}
-            columnKey={i + ele}
-            alignFrozen="center"
-            className="font-bold"
+            filterField={ele}
+            header={projectNameHeader(ele)}
+            columnKey={ele || i}
+            frozen={frozenCoulmns.includes(ele)}
+            alignFrozen="left"
+            className={frozenCoulmns.includes(ele) ? "font-bold" : ""}
+            showFilterMenu={false}
             // body={fullKitReadinessBody}
           />
         );
       });
     }
   };
-  const rowExpansionTemplate = (data) => {
-    return (
-      <div className="rowExpansionTemplate">
-        <DataTable
-          dataKey="Task"
-          value={data.SubTask}
-          scrollable
-          responsiveLayout="scroll"
-          loading={loading}
-        >
-          {rowExpansionColumns()}
-        </DataTable>
-      </div>
-    );
-  };
-
-  const getTreeTableNodesData = () => {
-    return [
-      {
-          key: '0',
-          data: {
-              name: 'Applications',
-              size: '100kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '0-0',
-                  data: {
-                      name: 'React',
-                      size: '25kb',
-                      type: 'Folder'
-                  },
-                  children: [
-                      {
-                          key: '0-0-0',
-                          data: {
-                              name: 'react.app',
-                              size: '10kb',
-                              type: 'Application'
-                          }
-                      },
-                      {
-                          key: '0-0-1',
-                          data: {
-                              name: 'native.app',
-                              size: '10kb',
-                              type: 'Application'
-                          }
-                      },
-                      {
-                          key: '0-0-2',
-                          data: {
-                              name: 'mobile.app',
-                              size: '5kb',
-                              type: 'Application'
-                          }
-                      }
-                  ]
-              },
-              {
-                  key: '0-1',
-                  data: {
-                      name: 'editor.app',
-                      size: '25kb',
-                      type: 'Application'
-                  }
-              },
-              {
-                  key: '0-2',
-                  data: {
-                      name: 'settings.app',
-                      size: '50kb',
-                      type: 'Application'
-                  }
-              }
-          ]
-      },
-      {
-          key: '1',
-          data: {
-              name: 'Cloud',
-              size: '20kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '1-0',
-                  data: {
-                      name: 'backup-1.zip',
-                      size: '10kb',
-                      type: 'Zip'
-                  }
-              },
-              {
-                  key: '1-1',
-                  data: {
-                      name: 'backup-2.zip',
-                      size: '10kb',
-                      type: 'Zip'
-                  }
-              }
-          ]
-      },
-      {
-          key: '2',
-          data: {
-              name: 'Desktop',
-              size: '150kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '2-0',
-                  data: {
-                      name: 'note-meeting.txt',
-                      size: '50kb',
-                      type: 'Text'
-                  }
-              },
-              {
-                  key: '2-1',
-                  data: {
-                      name: 'note-todo.txt',
-                      size: '100kb',
-                      type: 'Text'
-                  }
-              }
-          ]
-      },
-      {
-          key: '3',
-          data: {
-              name: 'Documents',
-              size: '75kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '3-0',
-                  data: {
-                      name: 'Work',
-                      size: '55kb',
-                      type: 'Folder'
-                  },
-                  children: [
-                      {
-                          key: '3-0-0',
-                          data: {
-                              name: 'Expenses.doc',
-                              size: '30kb',
-                              type: 'Document'
-                          }
-                      },
-                      {
-                          key: '3-0-1',
-                          data: {
-                              name: 'Resume.doc',
-                              size: '25kb',
-                              type: 'Resume'
-                          }
-                      }
-                  ]
-              },
-              {
-                  key: '3-1',
-                  data: {
-                      name: 'Home',
-                      size: '20kb',
-                      type: 'Folder'
-                  },
-                  children: [
-                      {
-                          key: '3-1-0',
-                          data: {
-                              name: 'Invoices',
-                              size: '20kb',
-                              type: 'Text'
-                          }
-                      }
-                  ]
-              }
-          ]
-      },
-      {
-          key: '4',
-          data: {
-              name: 'Downloads',
-              size: '25kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '4-0',
-                  data: {
-                      name: 'Spanish',
-                      size: '10kb',
-                      type: 'Folder'
-                  },
-                  children: [
-                      {
-                          key: '4-0-0',
-                          data: {
-                              name: 'tutorial-a1.txt',
-                              size: '5kb',
-                              type: 'Text'
-                          }
-                      },
-                      {
-                          key: '4-0-1',
-                          data: {
-                              name: 'tutorial-a2.txt',
-                              size: '5kb',
-                              type: 'Text'
-                          }
-                      }
-                  ]
-              },
-              {
-                  key: '4-1',
-                  data: {
-                      name: 'Travel',
-                      size: '15kb',
-                      type: 'Text'
-                  },
-                  children: [
-                      {
-                          key: '4-1-0',
-                          data: {
-                              name: 'Hotel.pdf',
-                              size: '10kb',
-                              type: 'PDF'
-                          }
-                      },
-                      {
-                          key: '4-1-1',
-                          data: {
-                              name: 'Flight.pdf',
-                              size: '5kb',
-                              type: 'PDF'
-                          }
-                      }
-                  ]
-              }
-          ]
-      },
-      {
-          key: '5',
-          data: {
-              name: 'Main',
-              size: '50kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '5-0',
-                  data: {
-                      name: 'bin',
-                      size: '50kb',
-                      type: 'Link'
-                  }
-              },
-              {
-                  key: '5-1',
-                  data: {
-                      name: 'etc',
-                      size: '100kb',
-                      type: 'Link'
-                  }
-              },
-              {
-                  key: '5-2',
-                  data: {
-                      name: 'var',
-                      size: '100kb',
-                      type: 'Link'
-                  }
-              }
-          ]
-      },
-      {
-          key: '6',
-          data: {
-              name: 'Other',
-              size: '5kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '6-0',
-                  data: {
-                      name: 'todo.txt',
-                      size: '3kb',
-                      type: 'Text'
-                  }
-              },
-              {
-                  key: '6-1',
-                  data: {
-                      name: 'logo.png',
-                      size: '2kb',
-                      type: 'Picture'
-                  }
-              }
-          ]
-      },
-      {
-          key: '7',
-          data: {
-              name: 'Pictures',
-              size: '150kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '7-0',
-                  data: {
-                      name: 'barcelona.jpg',
-                      size: '90kb',
-                      type: 'Picture'
-                  }
-              },
-              {
-                  key: '7-1',
-                  data: {
-                      name: 'primeng.png',
-                      size: '30kb',
-                      type: 'Picture'
-                  }
-              },
-              {
-                  key: '7-2',
-                  data: {
-                      name: 'prime.jpg',
-                      size: '30kb',
-                      type: 'Picture'
-                  }
-              }
-          ]
-      },
-      {
-          key: '8',
-          data: {
-              name: 'Videos',
-              size: '1500kb',
-              type: 'Folder'
-          },
-          children: [
-              {
-                  key: '8-0',
-                  data: {
-                      name: 'primefaces.mkv',
-                      size: '1000kb',
-                      type: 'Video'
-                  }
-              },
-              {
-                  key: '8-1',
-                  data: {
-                      name: 'intro.avi',
-                      size: '500kb',
-                      type: 'Video'
-                  }
-              }
-          ]
-      }
-  ];
-}
 
   return (
     <div className="myProjectAnddAllProjectList">
       <Suspense fallback={<div>Loading...</div>}>
         <Toast ref={toast} />
-
-        {/* <ConfirmationPopUp
-          onSort={onSort}
-          setProjectFrozen={setProjectFrozen}
-          saveSettings={saveSettings}
-          projectData={pegadata}
-          addFrozenColumns={addFrozenColumns}
-          onGlobalFilterChange={onGlobalFilterChange}
-          selectedColumnName={selectedColumnName}
-          ProjectFrozen={ProjectFrozen}
-          selectedCities={selectedCities}
-          setFrozenColumn={setFrozenColumn}
-          frozenCoulmns={frozenCoulmns}
-          sortData={sortData}
-          setSortData={setSortData}
-          setFilters={setFilters}
-          filters={filters}
-          op={op}
-          clearColumnWiseFilter={clearColumnWiseFilter}
-        /> */}
-        {/* <TreeTable
-          value={getTreeNodesData()}
-          tableStyle={{ minWidth: "50rem" }}
-        >
-          {dynamicColumns()}
-        </TreeTable> */}
         <div className="card">
-            <TreeTable value={getTreeTableNodesData()} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="name" header="Name" expander></Column>
-                <Column field="size" header="Size"></Column>
-                <Column field="type" header="Type"></Column>
-                <Column field="size" header="Size1"></Column>
-                <Column field="type" header="Type1"></Column>
-                <Column field="size" header="Size2"></Column>
-                <Column field="type" header="Type2"></Column>
-                <Column field="size" header="Size3"></Column>
-                <Column field="type" header="Type3"></Column>
-                <Column field="size" header="Size4"></Column>
-                <Column field="type" header="Type4"></Column>
-            </TreeTable>
+          <ConfirmationPopUp
+            onSort={onSort}
+            setProjectFrozen={setProjectFrozen}
+            saveSettings={saveSettings}
+            projectData={pegadata}
+            addFrozenColumns={addFrozenColumns}
+            onGlobalFilterChange={onGlobalFilterChange}
+            selectedColumnName={selectedColumnName}
+            ProjectFrozen={ProjectFrozen}
+            selectedCities={selectedCities}
+            setFrozenColumn={setFrozenColumn}
+            frozenCoulmns={frozenCoulmns}
+            sortData={sortData}
+            setSortData={setSortData}
+            setFilters={setFilters}
+            filters={filters}
+            op={op}
+            clearColumnWiseFilter={clearColumnWiseFilter}
+          />
+          <TreeTable
+            resizableColumns
+            dataKey="Task"
+            reorderableColumns
+            onColReorder={storeReorderedColumns}
+            onResize={(e) => console.log("resize", e)}
+            onResizeCapture={(e) => console.log("e", e)}
+            value={filters.length ? filters : pegadata}
+            scrollable
+            responsiveLayout="scroll"
+            loading={loading}
+            className="mt-3"
+            columnResizeMode="expand"
+            onColumnResizeEnd={onColumnResizeEnd}
+            filters={searchHeader}
+            filterDisplay={isSearch && "row"}
+            ref={dt}
+            tableStyle={{ minWidth: "50rem", tableLayout: "auto" }}
+          >
+            <Column header="" expander></Column>
+            {rowExpansionColumns()}
+          </TreeTable>
         </div>
-        {/* <DataTable
-          resizableColumns
-          dataKey="Task"
-          reorderableColumns
-          onColReorder={storeReorderedColumns}
-          onResize={(e) => console.log("resize", e)}
-          onResizeCapture={(e) => console.log("e", e)}
-          value={filters.length ? filters : pegadata}
-          scrollable
-          responsiveLayout="scroll"
-          loading={loading}
-          className="mt-3"
-          columnResizeMode="expand"
-          onColumnResizeEnd={onColumnResizeEnd}
-          filters={searchHeader}
-          filterDisplay={isSearch && "row"}
-          ref={dt}
-          tableStyle={{ minWidth: "50rem" }}
-          onRowToggle={(e) => setExpandedRows(e.data)}
-          expandedRows={expandedRows}
-          rowExpansionTemplate={rowExpansionTemplate}
-        >
-          <Column expander={allowExpansion} style={{ width: "5rem" }} />
-          {dynamicColumns()}
-        </DataTable> */}
       </Suspense>
     </div>
   );
