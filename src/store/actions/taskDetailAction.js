d99754a32db9c9eda2a6fd62d9b8c5823442ab58
sheetalNaukudkar <nnsheetal@gmail.com>
2023-05-25 20:18:43 +0530

    URDT pages changes

--- src/store/actions/taskDetailAction.js
+++ src/store/actions/taskDetailAction.js
@@ -29,25 +29,6 @@ export const getTaskDetails = (taskID, projectId) => {
   };
 };
 
-export const submitUploadApproveDesignIntent = async (formData, id, headers = {}) => {
-  const api = new Api();
-  const axiosInstance = await api.init({ headers });
-  let apiURL;
-  //https://pegadev.pg.com/prweb/api/ArtworkAgilityFile/v2/assignments/ASSIGN-WORKLIST PG-AAS-WORK U-5708!UPLOADAPPROVEDESIGNINTENT_FLOW/actions/UploadApproveDesignIntent
-  
-  apiURL = `${DEVURL}/v2/assignments/ASSIGN-WORKLIST ${id}!UPLOADAPPROVEDESIGNINTENT_FLOW/actions/UploadApproveDesignIntent`;
-
-  const submitUploadApproveDesignIntentData = await axiosInstance({
-    url: apiURL,
-    method: "PATCH",
-    data: formData,
-  });
-  // if (submitUploadApproveDesignIntentData?.data?.ID) {
-  //   App.dispatchToStore(editProjectAction(formData));
-  // }
-  return submitUploadApproveDesignIntentData;
-};
-
 export const saveDesignIntent = async (formData, headers = {}) => {
   const api = new Api();
   const axiosInstance = await api.init({ headers });
