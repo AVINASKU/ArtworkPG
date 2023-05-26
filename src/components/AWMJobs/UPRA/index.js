2ed7b0da2dc6656327764f80cbd2429b7bf3d912
pranali-ghalme <pranali.sambhaji-ghalme@capgemini.com>
2023-05-26 09:47:27 +0530

    upload file

--- src/components/AWMJobs/UPRA/index.js
+++ src/components/AWMJobs/UPRA/index.js
@@ -5,6 +5,15 @@ import FooterButtons from "../DesignJobs/FooterButtons";
 import CloneJobs from "../DesignJobs/CloneJobs";
 import { ProjectService } from "../../../service/PegaService";
 import ApproveDesignIntentContent from "../DesignJobs/ApproveDesignIntentContent";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getTaskDetails,
+  submitUploadApproveDesignIntent,
+} from "../../../store/actions/taskDetailAction";
+import AddNewDesign from "../DesignJobs/TaskHeader";
+import { UploadFileToServer } from "../../../store/actions/ProofScopeActions";
+
 
 const breadcrumb = [
   { label: "My Tasks", url: "/myTasks" },
@@ -14,17 +23,61 @@ const headerName = "Upload Production Ready Art";
 
 const UPRA = () => {
   const [data, setData] = useState(null);
+  const [designIntent, setDesignIntent] = useState(null);
   const [taskData, setTaskData] = useState(null);
-    const [fileName, setFileName] = useState("");
+  const [fileName, setFileName] = useState("");
   const [azureFile, setAzureFile] = useState("");
+  const [formattedValue, setformattedValue] = useState(0);
+  const [mappedFiles, setMappedFiles] = useState([]);
+
+  const { TaskDetailsData, loading } = useSelector(
+    (state) => state.TaskDetailsReducer
+  );
+  let { TaskID, ProjectID } = useParams();
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const id = `${TaskDetailsData?.ArtworkAgilityTasks[0]?.Task_Key}`;
+  const roleName = "DI_";
+  const version = "V1";
 
   useEffect(() => {
-    const data1 = ProjectService.getApproveDI();
-    setData(data1);
-    setTaskData(data1.DesignIntentList);
-  }, [data]);
+    dispatch(getTaskDetails(TaskID, ProjectID));
+  }, [dispatch, TaskID, ProjectID]);
 
-  console.log("file name and azure file", fileName,azureFile);
+  useEffect(() => {
+    if (TaskDetailsData) {
+      setDesignIntent(
+        TaskDetailsData?.ArtworkAgilityTasks[0]?.DesignJobDetails || []
+      );
+      setData(TaskDetailsData?.ArtworkAgilityTasks[0] || []);
+    }
+  }, [TaskDetailsData]);
+
+  console.log("file name and azure file", fileName, azureFile);
+  const filePath = "cloudflow://PP_FILE_STORE/aacdata/" + fileName;
+
+    const onSubmit = async () => {
+    const headers = {
+      key: 'If-Match',
+      value: TaskDetailsData?.ArtworkAgilityPage?.Etag,
+    };
+    console.log("azureFile", azureFile);
+    const formData = {
+      caseTypeID: 'PG-AAS-Work-UploadApprovedDesignIntent',
+      content: {
+        AWMTaskID: TaskDetailsData?.ArtworkAgilityTasks[0]?.Task_ID,
+        AWMProjectID: TaskDetailsData?.ArtworkAgilityPage?.AWM_Project_ID,
+        Size : formattedValue,
+        Version: version,
+        Filename: fileName
+      },
+    };
+    await dispatch(UploadFileToServer(azureFile, filePath));
+    console.log('formData', formData, "id", id);
+    // await submitUploadApproveDesignIntent(formData, id, headers);
+  };
+
 
   return (
     <PageLayout>
@@ -34,9 +87,26 @@ const UPRA = () => {
         disabled={true}
         label="Upload Production Ready Art"
       />
-      {/* {<CloneJobs {...data} />} */}
-      <ApproveDesignIntentContent {...taskData} upload={true} setAzureFile={setAzureFile} setFileName={setFileName} />
-      <FooterButtons />
+      {<AddNewDesign {...data} />}
+      {loading ? (
+        <i className="pi pi-spin pi-spinner" style={{ fontSize: "2rem" }}></i>
+      ) : (
+        designIntent && (
+          <ApproveDesignIntentContent
+            {...designIntent}
+            upload={true}
+            setformattedValue={setformattedValue}
+            setAzureFile={setAzureFile}
+            setFileName={setFileName}
+            setMappedFiles={setMappedFiles}
+            item={data}
+            roleName={roleName}
+            ArtworkAgilityPage={TaskDetailsData?.ArtworkAgilityPage}
+            version={version}
+          />
+        )
+      )}{" "}
+      <FooterButtons onSubmit={onSubmit} />
     </PageLayout>
   );
 };
