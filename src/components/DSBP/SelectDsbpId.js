aeb8a43f582c417cbc73f6c4ad892b620f7e3ec7
Pranali Ghalme <pranali.sambhaji-ghalme@capgemini.com>
2023-06-16 13:08:41 +0530

    dsbp select dropdown

--- src/components/DSBP/SelectDsbpId.js
+++ src/components/DSBP/SelectDsbpId.js
@@ -13,62 +13,77 @@ const SelectDsbpId = () => {
     { name: "Paris", code: "PRS" },
   ];
 
-    const selectedCityTemplate = (option) => {
+  const selectedCityTemplate = (option) => {
     return option.name;
   };
 
-   const cityOptionTemplate = (option) => {
+  const cityOptionTemplate = (option) => {
     return (
       <div className="city-option">
         <div className="city-name">{option.name}</div>
         <div>
-         <img
-              src={plusCollapseImg}
-              className="add-new-design-intent-icon"
-              alt=""
-              style={{height:12}}
-            />
-        </div>
-        <div> 
-        <img
-            src={deleteIcon}
-            alt="filter logo"
-            className="header-icons"
-            style={{height:12}}
+          <img
+            src={plusCollapseImg}
+            className={`add-new-design-intent-icon ${
+              selectedCities &&
+              selectedCities.length &&
+              selectedCities.includes(option.name) &&
+              "disable-icons"
+            }`}
+            alt=""
+            style={{ height: 12 }}
           />
-          </div>
-
+        </div>
+        <div>
+          <img
+            src={deleteIcon}
+            onClick={(e) => {
+              e.stopPropagation();
+              console.log("pranali", option, selectedCities);
+              if (selectedCities.includes(option.name)) {
+                const updatedCities = selectedCities.filter(
+                  (item) => item !== option.name
+                );
+                setSelectedCities(updatedCities);
+              }
+              // alert("clicked",option, selectedCities);
+            }}
+            alt="filter logo"
+            className={`header-icons ${
+              ((selectedCities && !selectedCities.includes(option.name)) ||
+                !selectedCities) &&
+              "disable-icons"
+            }`}
+            style={{ height: 12 }}
+          />
+        </div>
       </div>
     );
   };
 
-  const customStyles = {
-    '.p-checkbox': {
-      display: 'none',
-      // Add any other custom styles you want to override
-    },
+  const multiSelectOnChange = (e) => {
+    setSelectedCities(e.value);
   };
-
 
   return (
     <>
       <div className="margin-left">Select DSBP ID</div>
 
       <div className="actions multiselect-padding margin-left dsbp-select">
-            <MultiSelect
-      value={selectedCities}
-      onChange={(e) => setSelectedCities(e.value)}
-      options={cities}
-      optionLabel="name"
-      optionValue="name" // Set optionValue to "name" to remove checkboxes
-      placeholder="Select Cities"
-      filter
-      valueTemplate={selectedCityTemplate}
-      itemTemplate={cityOptionTemplate}
-      maxSelectedLabels={3}
-      className="dsbp-select1"
-      style={{ maxWidth: 330, width: "300%" }}
-    />
+        <MultiSelect
+          value={selectedCities}
+          onChange={(e) => multiSelectOnChange(e)}
+          options={cities}
+          optionLabel="name"
+          optionValue="name" // Set optionValue to "name" to remove checkboxes
+          placeholder="Select Cities"
+          filter
+          valueTemplate={selectedCityTemplate}
+          itemTemplate={cityOptionTemplate}
+          maxSelectedLabels={3}
+          panelClassName="dsbp-multiselect-dropdown"
+          style={{ maxWidth: 330, width: "300%" }}
+        />
 
         <div className="action-buttons margin-right">
           <div>DSBP ID's : 00</div>
