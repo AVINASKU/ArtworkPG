48aa9e77f40f3de0d8256fbb6e9c4a106b7a72d6
Pranali Ghalme <pranali.sambhaji-ghalme@capgemini.com>
2023-06-20 12:33:31 +0530

    import bug

--- src/components/DSBP/index.js
+++ src/components/DSBP/index.js
@@ -1,6 +1,5 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import ArtworkHeader from "./ArtworkHeader";
 import SelectDsbpId from "./SelectDsbpId";
 import ProjectNameHeader from "./ProjectNameHeader";
