8d3cfe0afa52ff888b227e4ff615c4997068121d
Pranali Ghalme <pranali.sambhaji-ghalme@capgemini.com>
2023-06-28 19:35:35 +0530

    Navlink added

--- src/components/DSBP/ProjectNameHeader.js
+++ src/components/DSBP/ProjectNameHeader.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const ProjectNameHeader = ({ selectedProjectDetails }) => {
   return (
     <div className=" margin-left" style={{ textAlign: "initial" }}>
+    
+      <NavLink to={`/myProjects`} className="p-menuitem-link">
+        <span
+          className="p-menuitem-text"
+          style={{ color: "#003DA5", fontWeight: 600, fontSize: 16 }}
+        >
+          {selectedProjectDetails?.Project_Name}
+        </span>
+      </NavLink>
+
+      {/* <NavLink to={`/myProjects`} className="p-menuitem-link">
       <span style={{ color: "#003DA5", fontWeight: 600, fontSize: 16 }}>
         {selectedProjectDetails?.Project_Name}
       </span>
+      </NavLink> */}
     </div>
   );
 };
