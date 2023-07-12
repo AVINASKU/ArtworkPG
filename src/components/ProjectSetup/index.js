f232a23af4b1f7ab431454fdcee001a9fd4cf0b8
bonugopal <bonugopal@capgemini.com>
2023-07-12 18:58:27 +0530

    Accordion-added for ProjectPlan.

--- src/components/ProjectSetup/index.js
+++ src/components/ProjectSetup/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState, useRef } from "react";
 import AddProject from "../Projects/CreateProject";
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { DropdownButton, Dropdown, Accordion } from "react-bootstrap";
 import "primeicons/primeicons.css";
 import "./index.scss";
 import { useSelector } from "react-redux";
@@ -195,7 +195,31 @@ function ProjectSetup(props) {
 
           {toggleButtons === "Tabular" && (
             <div>
-              <nav className="subNav">
+              <Accordion className="projectPlanAccordian" defaultActiveKey="2">
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>Design</Accordion.Header>
+                  <Accordion.Body>
+                    <ProjectPlanCompo
+                      isSearch={isSearch}
+                      setColWidth={setColWidth}
+                      childFunc={childFunc}
+                      test={test}
+                      tabNameForPP={tabNameForPP}
+                      view={toggleButtons}
+                      setTabName={setTabName}
+                    />
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>Input</Accordion.Header>
+                  <Accordion.Body>Input</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                  <Accordion.Header>FA Assembly</Accordion.Header>
+                  <Accordion.Body>FA Assembly</Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              {/* <nav className="subNav">
                 <div className="nav nav-tabs" id="nav-tab" role="tablist">
                   <button
                     className={`nav-link ${
@@ -243,10 +267,10 @@ function ProjectSetup(props) {
                     FA Assembly
                   </button>
                 </div>
-              </nav>
+              </nav> */}
             </div>
           )}
-          <div className="tab-content" id="nav-tabContent">
+          {/* <div className="tab-content" id="nav-tabContent">
             <div
               className={`tab-pane fade ${
                 tabNameForPP === "Design" ? "show active" : ""
@@ -293,7 +317,7 @@ function ProjectSetup(props) {
             >
               FA Assembly Data
             </div>
-          </div>
+          </div> */}
         </div>
       ),
     },
