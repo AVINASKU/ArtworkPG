57a87afe7da4c73d487b23c97895d88c1cf70ba2
sheetalNaukudkar <sheetal-chalu.navkudkar@capgemini.com>
2023-07-28 14:30:47 +0530

    added CIC task in project pan page

--- src/components/ProjectSetup/index.js
+++ src/components/ProjectSetup/index.js
@@ -219,6 +219,16 @@ function ProjectSetup(props) {
         code: "UBD",
         data: apiData.filter((data) => data.AWM_Task_ID.includes("UBD_")),
       },
+      {
+        name: "Approve CIC",
+        code: "ACIC",
+        data: apiData.filter((data) => data.AWM_Task_ID.includes("ACIC_")),
+      },
+      {
+        name: "Upload CIC",
+        code: "UCIC",
+        data: apiData.filter((data) => data.AWM_Task_ID.includes("UCIC_")),
+      },
     ];
     // }
 
@@ -233,7 +243,9 @@ function ProjectSetup(props) {
           task.data[0]?.AWM_Task_ID.includes("DNIQ_") ||
           task.data[0]?.AWM_Task_ID.includes("SAA_") ||
           task.data[0]?.AWM_Task_ID.includes("UBD_") ||
-          task.data[0]?.AWM_Task_ID.includes("DM_")
+          task.data[0]?.AWM_Task_ID.includes("DM_") ||
+          task.data[0]?.AWM_Task_ID.includes("ACIC_") ||
+          task.data[0]?.AWM_Task_ID.includes("UCIC_")
         ) {
           let tempObj = {};
           tempObj["key"] = task.data[0]?.AWM_Task_ID;
