af592c524d3ae5dc70e7fbb8dce34f767f6903a7
shivu-capgemini <141023529+shivu-capgemini@users.noreply.github.com>
2023-08-08 20:31:20 +0530

    GoJs Implementation

--- src/components/Projects/AllProjects/index.js
+++ src/components/Projects/AllProjects/index.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import PageLayout from "../../PageLayout";
 import AllProjectList from "./AllProjectList";
+import GojsComponent from "../../../gojs";
 
 function AllProjects() {
   return (
     <PageLayout>
+      <GojsComponent />
       <div className="content-layout" id="tableDiv">
         <div className="tabular-view">
           <AllProjectList header="All Projects" /> 
