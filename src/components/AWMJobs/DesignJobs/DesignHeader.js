d626a48eab2a57263237b067992fb99bad9ca774
bonugopal <bonugopal@capgemini.com>
2023-08-16 16:38:36 +0530

    QA Defects-Breadcrum issues are fixed.

--- src/components/AWMJobs/DesignJobs/DesignHeader.js
+++ src/components/AWMJobs/DesignJobs/DesignHeader.js
@@ -46,7 +46,9 @@ const DesignHeader = ({
                   {" "}
                   <li className="">
                     <NavLink to={`/${mytasks}`} className="p-menuitem-link">
-                      <span className="p-menuitem-text">{mytasks}</span>
+                      <span className="p-menuitem-text">
+                        {mytasks === "myProjects" ? "My Projects" : mytasks}
+                      </span>
                     </NavLink>{" "}
                   </li>{" "}
                   <li className="p-breadcrumb-chevron pi pi-chevron-right"></li>
@@ -55,14 +57,18 @@ const DesignHeader = ({
                       to={`/${mytasks}/${url[2]}/${url[5]}`}
                       className="p-menuitem-link"
                     >
-                      <span className="p-menuitem-text">{url[2]}</span>
+                      <span className="p-menuitem-text">
+                        {url[2] === "projectPlan" ? "Project Plan" : url[2]}
+                      </span>
                     </NavLink>{" "}
                   </li>
                 </>
               ) : (
                 <li className="">
                   <NavLink to={`/${mytasks}`} className="p-menuitem-link">
-                    <span className="p-menuitem-text">{mytasks}</span>
+                    <span className="p-menuitem-text">
+                      {mytasks === "allTasks" ? "All Tasks" : mytasks}
+                    </span>
                   </NavLink>{" "}
                 </li>
               )}
